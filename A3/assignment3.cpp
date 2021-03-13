d00ed63c93ad3724a246cda9cca297f6396f3135
finvic <cs1190332@iitd.ac.in>
2021-03-13 22:27:46 +0530

    Done ;)

--- A3/assignment3.cpp
+++ A3/assignment3.cpp
@@ -29,26 +29,23 @@ int n_total;
 int INSTRUCTION_MEMORY = pow(2,17); // Memory is word Addressable hence it has 2^19 Bytes
 int DATA_MEMORY = pow(2,17);
 
-
+// This code removes trailing and starting whitespace characters
 const std::string WHITESPACE = " \n\r\t\f\v";
- 
 std::string ltrim(const std::string& s)
 {
     size_t start = s.find_first_not_of(WHITESPACE);
     return (start == std::string::npos) ? "" : s.substr(start);
 }
- 
 std::string rtrim(const std::string& s)
 {
     size_t end = s.find_last_not_of(WHITESPACE);
     return (end == std::string::npos) ? "" : s.substr(0, end + 1);
 }
- 
 std::string trim(const std::string& s)
 {
     return rtrim(ltrim(s));
 }
-
+//Checks if the input string is present in the register or not
 void validator(vector<string> V, string s,int l){
     for( auto j : V){
         if (j == s){
@@ -59,6 +56,7 @@ void validator(vector<string> V, string s,int l){
     exit(-1);
 }
 
+// Prints the registers in decimal format
 void print(){
     out<<"R0 [zero] = "<<reg["$zero"]<<endl;
     out<<"R1  [r1]  = "<<reg["$r1"]<<endl;
@@ -94,13 +92,14 @@ void print(){
     out<<"R31 [r31] = "<<reg["$r31"]<<endl<<endl;
 }
 
+//Converts decimal to hexadecimal
 string dectohex(int n){
     stringstream ss; 
     ss << hex << n; 
     string res = ss.str(); 
     return res;    
 }
-
+//prints the registers in hexadecimal format
 void print_hex(){
     out<<"R0 [zero] = "<<dectohex(reg["$zero"])<<endl;
     out<<"R1  [r1]  = "<<dectohex(reg["$r1"])<<endl;
@@ -135,7 +134,7 @@ void print_hex(){
     out<<"R30 [r30] = "<<dectohex(reg["$r30"])<<endl;
     out<<"R31 [r31] = "<<dectohex(reg["$r31"])<<endl<<endl;
 }
-
+// Validator for the operands of instructions "add","sub","mul","addi","slt"
 //Operator $reg1,reg2,reg3/Int
 void validator_add(string s, int l, string instruction){
         stringstream s_stream(s);
@@ -148,6 +147,11 @@ void validator_add(string s, int l, string instruction){
                     if(!regex_match(substr,n)){
                         cout<<"Invalid second operator "<<substr<<"for operator "<<instruction<<endl;
                         exit(-1);
+                    }else{
+                        if (abs(stoi(substr))>pow(2,16)){
+                            cout<<"Error! I-type instruction cannot exceed 2^16 in MIPS on line "<<l+1<<endl;
+                            exit(-1); 
+                        }
                     }
             }else if (count == 2){
                 if(substr[0] =='$'){validator(registers,substr,l+1);}
@@ -155,6 +159,11 @@ void validator_add(string s, int l, string instruction){
                     if(!regex_match(substr,n)){
                         cout<<"Invalid second operator "<<substr<<"for operator "<<instruction<<endl;
                         exit(-1);
+                    }else{
+                        if (abs(stoi(substr))>pow(2,16)){
+                            cout<<"Error! I-type instruction cannot exceed 2^16 in MIPS on line "<<l+1<<endl;
+                            exit(-1); 
+                        }
                     }
                 }
             }
@@ -167,6 +176,7 @@ void validator_add(string s, int l, string instruction){
             exit(-1);
         }
 }
+// Validator for instructions "bne" and "beq"
 // beq $reg1,$reg2/Int,label
 void validator_beq(string s, int l, string instruction){
         stringstream s_stream(s);   
@@ -182,6 +192,11 @@ void validator_beq(string s, int l, string instruction){
                     if(!regex_match(substr,n)){
                         cout<<"Invalid second operator "<<substr<<"for operator "<<instruction<<endl;
                         exit(-1);
+                    }else{
+                        if (abs(stoi(substr))>pow(2,16)){
+                            cout<<"Error! I-type instruction cannot exceed 2^16 in MIPS on line "<<l+1<<endl;
+                            exit(-1); 
+                        }
                     }
                 }
             }
@@ -193,6 +208,7 @@ void validator_beq(string s, int l, string instruction){
             exit(-1);
         }
 }
+//Validator for Instructions "lw" and "sw"
 //lw $reg1,off($reg2)
 void validator_lw(string s, int l, string instruction){
 
@@ -233,12 +249,11 @@ void validator_lw(string s, int l, string instruction){
 
 
 int main(int argc, char *argv[]) {
-	// your code goes here
-
     file1.open(argv[1]);
+    if(!file1.is_open()){cout<<"File not found"<<endl;exit(-1);}
     string input_file = argv[1];
     out.open(input_file+"_output.txt");
-    vector<string> instructions;
+    vector<string> instructions; //Vector containing all instructions 
     string line;
     int num=0;
     size_t x;
@@ -253,10 +268,8 @@ int main(int argc, char *argv[]) {
         }
         x = line.find_first_of('$');
         Instruction = line;
-        //cout<<(string::npos);
-        //cout<<(x==string::npos);
         if(x==string::npos){
-            
+            // The line does not contain $ symbol hence it can either be a label or a jump instruction
             if (Instruction.substr(0,2) == "j "){
                 params[num].push_back("j");
                 string oprd = trim(Instruction.substr(2));
@@ -264,6 +277,11 @@ int main(int argc, char *argv[]) {
             }else if(Instruction.back() == ':'){
                 string b = trim(Instruction.substr(0,Instruction.size()-1));
                 if (regex_match(b,l)){
+                    if(label.find(b) != label.end()){
+                        cout<<"Same Label can't be defined twice"<<endl;
+                        cout<<"Label "<<b<<" is defined on lines "<<label[b]+1<<" and "<<num+1<<endl;
+                        exit(-1);
+                    }
                      label[b] = num;
                      params[num].push_back(b);
                  }else{
@@ -275,9 +293,8 @@ int main(int argc, char *argv[]) {
                 exit(-1);
             }
         }else{
-            
-            Instruction = trim(line.substr(0,x));  
-            //cout<<Instruction<<endl;          
+            // The line contains $ symbol
+            Instruction = trim(line.substr(0,x));           
             validator(operations,Instruction,num+1);           
             params[num].push_back(Instruction);
             string operands = trim(line.substr(x));            
@@ -286,14 +303,13 @@ int main(int argc, char *argv[]) {
             }else if (Instruction=="beq"||Instruction =="bne"){
                 validator_beq(operands,num,Instruction);
             }else if (Instruction == "lw"||Instruction =="sw"){
-                
                 validator_lw(operands,num,Instruction);
             }else{
                 continue;
             }
         }
-        //cout<<num<<" "<<INSTRUCTION_MEMORY<<endl;
         num++;
+        // Throws an error if number of commands is more than Instruction storage limit.
         if (num>INSTRUCTION_MEMORY){
             cout<<"Error! Instruction memory limit exceeded"<<endl;
             exit(-1);
@@ -301,7 +317,7 @@ int main(int argc, char *argv[]) {
         // instruction memory nums X 4 bytes
     }
     file1.close();
-// PARSING ENDS ------------------------------------------------------------------------------------
+// -----------------------------------------------PARSING ENDS ---------------------------------------------------------
     // for (auto const& j : params){
     //      int c = j.first;
     //      vector<string> v = j.second;
@@ -316,10 +332,10 @@ int main(int argc, char *argv[]) {
     // }
     // cout<<endl;
     int pc = 0;
+    // Program Counter that iterates over the instruction set and points to the instruction being executed
     while(pc<num){
         if(instructions[pc]==""){pc++;continue;}
         n_total++;
-        //cout<<pc<<endl;
         out<<"Integer Register : "<<pc+1<<"   "<<instructions[pc]<<" \n"<<endl;
         Instruction=params[pc][0];
         statistics[Instruction] ++;
@@ -341,7 +357,6 @@ int main(int argc, char *argv[]) {
                 exit(-1);
             }
             int y = (params[pc][2][0]=='$') ? reg[params[pc][2]] : stoi(params[pc][2]);
-            //cout<<"\n"<<y<<" "<<reg[params[pc][1]];
             if (reg[params[pc][1]] == y){
                 pc = label[params[pc][3]];
                 print_hex();
@@ -383,7 +398,6 @@ int main(int argc, char *argv[]) {
             reg[params[pc][1]] = data_memory[(offset+reg[params[pc][3]])];
             pc++;           
         } else if (Instruction=="sw"){
-        // 0 -1 , 4 - 189, 8 - 2537
             int offset = stoi(params[pc][2]);
             if((offset+reg[params[pc][3]])%4 != 0 || (offset+reg[params[pc][3]])<0  || (offset+reg[params[pc][3]])>DATA_MEMORY){
                 cout<<"Program tried to access invalid memory location."<<endl;
@@ -399,9 +413,14 @@ int main(int argc, char *argv[]) {
              n_total --;
         }else{
             continue;
-        }     
+        }
+        if(reg["$zero"] != 0){
+            cout<<"The value in Zero Registor is not mutable. Command on line "<<pc<<" tries to change its value"<<endl;
+            exit(-1);
+        }
         print_hex();
     }
+    // Prints the relevant statistics of the MIPS code. 
     cout<<"Number of clock cycles "<<n_total<<endl;
     out<<"Number of clock cycles "<<n_total<<endl;
     cout<<"Instruction Memory Used "<<num*4 <<" Bytes"<<endl;
@@ -419,3 +438,4 @@ int main(int argc, char *argv[]) {
     out.close();
 	return 0;
 }
+//------------------------------------Execution Ends---------------------------------------------------------------------
