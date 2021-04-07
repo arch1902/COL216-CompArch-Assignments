fe74a3b92e609def70e961b83a27a496ab4abe97
Aman Gupta <amangupta@Amans-MBP.bbrouter>
2021-04-07 23:43:43 +0530

    basic working model

--- A4/assignment4.cpp
+++ A4/assignment4.cpp
@@ -70,37 +70,37 @@ void validator(vector<string> V, string s,int l){
 
 // Prints the registers in decimal format
 void print(){
-    out<<reg["$r1"];
-    out<<" "<<reg["$r2"];
-    out<<" "<<reg["$r3"];
-    out<<" "<<reg["$r4"];
-    out<<" "<<reg["$r5"];
-    out<<" "<<reg["$r6"];
-    out<<" "<<reg["$r7"];
-    out<<" "<<reg["$r8"];
-    out<<" "<<reg["$r9"];
-    out<<" "<<reg["$r10"];
-    out<<" "<<reg["$r11"];
-    out<<" "<<reg["$r12"];
-    out<<" "<<reg["$r13"];
-    out<<" "<<reg["$r14"];
-    out<<" "<<reg["$r15"];
-    out<<" "<<reg["$r16"];
-    out<<" "<<reg["$r17"];
-    out<<" "<<reg["$r18"];
-    out<<" "<<reg["$r19"];
-    out<<" "<<reg["$r20"];
-    out<<" "<<reg["$r21"];
-    out<<" "<<reg["$r22"];
-    out<<" "<<reg["$r23"];
-    out<<" "<<reg["$r24"];
-    out<<" "<<reg["$r25"];
-    out<<" "<<reg["$r26"];
-    out<<" "<<reg["$r27"];
-    out<<" "<<reg["$r28"];
-    out<<" "<<reg["$r29"];
-    out<<" "<<reg["$r30"];
-    out<<" "<<reg["$r31"]<<endl;
+    out<<"zero = "<<reg["$zero"]<<endl;
+    out<<"r1 =  "<<reg["$r1"]<<endl;
+    out<<"r2 = "<<reg["$r2"]<<endl;
+    out<<"r3 =  "<<reg["$r3"]<<endl;
+    out<<"r4 = "<<reg["$r4"]<<endl;
+    out<<"r5 = "<<reg["$r5"]<<endl;
+    out<<"r6 = "<<reg["$r6"]<<endl;
+    out<<"r7 = "<<reg["$r7"]<<endl;
+    out<<"r8 = "<<reg["$r8"]<<endl;
+    out<<"r9 = "<<reg["$r9"]<<endl;
+    out<<"s0 = "<<reg["$s0"]<<endl;
+    out<<"s1 = "<<reg["$s1"]<<endl;
+    out<<"s2 = "<<reg["$s2"]<<endl;
+    out<<"s3 = "<<reg["$s3"]<<endl;
+    out<<"s4 = "<<reg["$s4"]<<endl;
+    out<<"s5 = "<<reg["$s5"]<<endl;
+    out<<"s6 = "<<reg["$s6"]<<endl;
+    out<<"s7 = "<<reg["$s7"]<<endl;
+    out<<"s8 = "<<reg["$s8"]<<endl;
+    out<<"s9 = "<<reg["$s9"]<<endl;
+    out<<"t0 = "<<reg["$t0"]<<endl;
+    out<<"t1 = "<<reg["$t1"]<<endl;
+    out<<"t2 = "<<reg["$t2"]<<endl;
+    out<<"t3 = "<<reg["$t3"]<<endl;
+    out<<"t4 = "<<reg["$t4"]<<endl;
+    out<<"t5 = "<<reg["$t5"]<<endl;
+    out<<"t6 = "<<reg["$t6"]<<endl;
+    out<<"t7 = "<<reg["$t7"]<<endl;
+    out<<"t8 = "<<reg["$t8"]<<endl;
+    out<<"t9 = "<<reg["$t9"]<<endl;
+    out<<"sp = "<<reg["$sp"]<<endl;
 }
 
 
@@ -163,7 +163,7 @@ void validator_beq(string s, int l, string instruction){
                         cout<<"Invalid second operator "<<substr<<"for operator "<<instruction<<endl;
                         exit(-1);
                     }else{
-                        if (abs(stoi(substr))>pow(2,16)){
+                        if (abs(stol(substr))>pow(2,16)){
                             cout<<"Error! I-type instruction cannot exceed 2^16 in MIPS on line "<<l+1<<endl;
                             exit(-1); 
                         }
@@ -200,6 +200,10 @@ void validator_lw(string s, int l, string instruction){
                     params[l].push_back("0");
                 }
                 else if (regex_match(lh,n)){
+                    if (abs(stoll(lh))>pow(2,16)){
+                        cout<<"Error! I-type instruction cannot exceed 2^16 in MIPS on line "<<l+1<<endl;
+                        exit(-1); 
+                    }
                     params[l].push_back(lh);
                 }else{cout<<"Invalid Operand for "<<instruction<<" on line "<<l+1<<endl;exit(-1);}
                 //(register) 
@@ -293,6 +297,25 @@ void j(int i){
     out <<instructions[i]<<endl;
     pc = label[params[i][1]];
 }
+
+void sort_queue(){
+    if (Dram_queue.empty()){return;}
+    vector<string> first = Dram_queue[0];
+    int row = stoi(first[1])/1024;
+    //[1,2,3,1,3,4,2,1,3,4,1]
+    //[1,1,1,1,2,3,3,4,2,]
+    int off = 1;
+    for(int i = 1;i<Dram_queue.size();i++){
+        vector<string> curr = Dram_queue[i];
+        int r = stoi(curr[1])/1024;
+        if(r==row){
+            Dram_queue.erase(Dram_queue.begin()+i);
+            Dram_queue.insert(off+Dram_queue.begin(),curr);
+            off++;
+        }
+    }
+}    
+
 int main(int argc, char *argv[]) {
     reg["$zero"] = 0;
     for(int i = 0;i<256;i++){Row_buffer.push_back(0);}
@@ -372,29 +395,36 @@ int main(int argc, char *argv[]) {
     //      }
     //      cout<<"\n";
     // }
-    // for(auto const& j : label){
-    //      cout<<j.first<<"->"<<j.second<<"   ";
-    // }
+    for(auto const& j : label){
+          cout<<j.first<<"->"<<j.second<<"   ";
+    }
     // cout<<endl;
     // Program Counter that iterates over the instruction set and points to the instruction being executed
     while (true){
         if (pc>=num && Dram_queue.empty()){break;}
+        if (pc<num && label.find(params[pc][0])!=label.end()){pc++;continue;}
         clock_cycle++;
-        out<<"Clock Cycle"<<clock_cycle<<":"<<endl;
-
+        out<<"\n"<<"Clock Cycle"<<clock_cycle<<":"<<endl;
+        bool flag = false;
+        string removed_register;
         //DRAM 
         if (!Dram_queue.empty()){
             vector<string> Curr_executed = Dram_queue[0];
             int address = stoi(Curr_executed[1]);
-            int row = address/512;
-            int  col = address%512;
+            int row = address/1024;
+            int  col = address%1024;
             if(address>INSTRUCTION_MEMORY || col%4 !=0){
                 cout<<"Program tried to access invalid memory address "<<address<<endl;
                 exit(-1);
             }
+            for(auto k: Dram_queue[0]){
+                 cout<<k<<" ";
+            }
+            cout<<endl;
             while(true){
-                Curr_executed[3] = to_string(stoi(Curr_executed[3])+1);
-                if (Curr_executed[3]=="1"){
+                //out<<Dram_queue[0][3]<<endl;0
+                if (Dram_queue[0][3]=="0"){
+                    out<<"Started "<<Dram_queue[0][0]<<" "<<Dram_queue[0][1]<<" "<<Dram_queue[0][2]<<endl;
                     int curr = 0;
                     if(last_buffer_row!=row){
                         curr += ROW_ACCESS_DELAY;
@@ -403,61 +433,76 @@ int main(int argc, char *argv[]) {
                         }
                     }
                     curr += COL_ACCESS_DELAY;
-                    Curr_executed[4] = to_string(curr);
-                    Curr_executed[3] = "2";
+                    Dram_queue[0][4] = to_string(curr);
+                    Dram_queue[0][3] = "1";
+                    out<<"Completed "<<"1"<<"/"<<Dram_queue[0][4]<<endl;
                     break;
                 }
-                if (Curr_executed[3]==to_string(ROW_ACCESS_DELAY)){
-                    if(Curr_executed[4]==to_string(2*ROW_ACCESS_DELAY+COL_ACCESS_DELAY)){
+                out<<"Completed "<<to_string(stoi(Dram_queue[0][3])+1)<<"/"<<Dram_queue[0][4]<<endl;
+                if (stoi(Dram_queue[0][3])==ROW_ACCESS_DELAY-1){
+                    if(Dram_queue[0][4]==to_string(2*ROW_ACCESS_DELAY+COL_ACCESS_DELAY)){
                         for (int j = 0;j<256;j++){
                             if (Dram_Memory[last_buffer_row][j] != Row_buffer[j]){
-                                out<<"Memory at "<<row*256+col<<" = "<<to_string(Row_buffer[j])<<endl;
+                                out<<"Memory at "<<last_buffer_row*1024+j*4<<" = "<<to_string(Row_buffer[j])<<endl;
                             }
                         }
                         Dram_Memory[last_buffer_row] = Row_buffer;
                     }
-                    if(Curr_executed[4]==to_string(ROW_ACCESS_DELAY+COL_ACCESS_DELAY)){
+                    if(stoi(Dram_queue[0][4])==ROW_ACCESS_DELAY+COL_ACCESS_DELAY){
                         Row_buffer = Dram_Memory[row];
                         last_buffer_row = row;
                     }
+                    Dram_queue[0][3] = to_string(stoi(Curr_executed[3])+1);
                     break;
                 }
-                if (Curr_executed[3]==to_string(2*ROW_ACCESS_DELAY) && Curr_executed[4]==to_string(2*ROW_ACCESS_DELAY+COL_ACCESS_DELAY)){
+                if (stoi(Dram_queue[0][3])==2*ROW_ACCESS_DELAY-1 && Dram_queue[0][4]==to_string(2*ROW_ACCESS_DELAY+COL_ACCESS_DELAY)){
                     Row_buffer = Dram_Memory[row];
                     last_buffer_row = row;
+                    Dram_queue[0][3] = to_string(stoi(Curr_executed[3])+1);
                     break;
                 }
 
-                if(Curr_executed[3]==Curr_executed[4]){
-                    if(Curr_executed[0]=="lw"){
-                        reg[Curr_executed[2]] = Row_buffer[col/4];
-                        Blocking_registers[Curr_executed[2]] -=1;
+                if(stoi(Dram_queue[0][3])==stoi(Dram_queue[0][4])-1){
+                    if(Dram_queue[0][0]=="lw"){
+                        reg[Dram_queue[0][2]] = Row_buffer[col/4];
+                        flag  = true;
+                        removed_register = Dram_queue[0][2];
+                        out<<removed_register<<" = "<<Row_buffer[col/4]<<endl;
+                        //Blocking_registers[Dram_queue[0][2]] -=1;
                     }
-                    if(Curr_executed[0] =="sw"){
-                        Row_buffer[col/4] = stoi(Curr_executed[2]);
+                    if(Dram_queue[0][0] =="sw"){
+                        Row_buffer[col/4] = stoi(Dram_queue[0][2]);
                     }
+                    out<<"Finished Instruction "<<Dram_queue[0][0]<<" "<<Dram_queue[0][1]<<" "<<Dram_queue[0][2]<<endl;
                     Dram_queue.erase(Dram_queue.begin());
+                    //Dram_queue[0][3] = to_string(stoi(Dram_queue[0][3])+1);
                     break;
                 }
+                Dram_queue[0][3] = to_string(stoi(Dram_queue[0][3])+1);
                 break;
             }
         }
         //Normal Commands 
         if (checker(pc)){
             Instruction = params[pc][0];
+            cout<<Instruction<<endl;
             if (Instruction == "lw"){
                 string relevant_registor = params[pc][1];
                 int offset = stoi(params[pc][2]);
                 int address = offset+reg[params[pc][3]];
-                Dram_queue.push_back({"lw",to_string(address),relevant_registor,"0","0"});   
-                Blocking_registers[relevant_registor] ++;        
+                Dram_queue.push_back({"lw",to_string(address),relevant_registor,"0","0"}); 
+                sort_queue();  
+                Blocking_registers[relevant_registor] ++;
+                out<< "Intialised instruction "<<"lw "<<address<<" "<<relevant_registor<<endl;        
                 pc++;
                 continue;
             }else if(Instruction == "sw"){
                 string relevant_registor = params[pc][1];
                 int offset = stoi(params[pc][2]);
                 int address = offset+reg[params[pc][3]];
-                Dram_queue.push_back({"lw",to_string(address),to_string(reg[relevant_registor]),"0","0"});           
+                Dram_queue.push_back({"sw",to_string(address),to_string(reg[relevant_registor]),"0","0"});  
+                sort_queue();
+                out<< "Intialised instruction "<<"sw "<<address<<" "<<reg[relevant_registor]<<endl;         
                 pc++;
                 continue;
             }else if (Instruction == "add"){ 
@@ -494,9 +539,21 @@ int main(int argc, char *argv[]) {
                 continue;
             }
         }
-
-
+        if (flag){
+            Blocking_registers[removed_register] -=1;
+            flag = false;
+        }
     }
+    out<<"\n"<<"DRAM memory structure :"<<endl;
+    for(int j = 0;j<512;j++){
+        for(int i =0;i<256;i++){
+            if (Dram_Memory[j][i] != 0){
+                out<<"Memory at row "<<j<<" column "<<i<<" address "<<j*1024+i*4<<" = "<<Dram_Memory[j][i]<<endl;
+            }
+        }
+    }
+    out<<"\n"<<"Register Values :"<<endl;
+    print();
 	return 0;
 }
 //------------------------------------Execution Ends---------------------------------------------------------------------
