0cfc4b8b18efdb9a3739221e021d46e78a84f699
Aman Gupta <amangupta@Amans-MBP.bbrouter>
2021-04-28 20:55:06 +0530

    Confused

--- A5/assgn5.cpp
+++ A5/assgn5.cpp
@@ -44,12 +44,17 @@ string curr_cpu_blocking_register = "";
 vector<vector<int>> Dram_Memory;
 vector<map<int,int>> all_lines_numbers;
 vector<int> Row_buffer;
-vector<vector<string>> Dram_queue;
+vector<vector<vector<string>>> Dram_queue;
 // map<string,int>  Blocking_registers;
 vector<map<string,int>> all_blocking_registers;
 vector<int> num_array;
-
+bool request_issued;
 int total_int_executed=0;
+int rows_for_a_core;
+int TOTAL_QUEUE_SIZE = 64;
+int individual_queue_size;
+int pushed_from_core;
+int executing_core;
 
 // This code removes trailing and starting whitespace characters
 const std::string WHITESPACE = " \n\r\t\f\v";
@@ -301,62 +306,12 @@ void jump(int i, int f){
     all_pc[f] = all_labels[f][all_params[f][i][1]];
 }
 
-void sort_queue(){
-    if (Dram_queue.empty()){return;}
-    vector<string> first = Dram_queue[0];
-    int row = (stoi(first[1])+CORE_MEMORY*stoi(first[6]))/1024;
-    // int blocking_row = -1;
-    // int blocking_column = -1;
-    // bool is_first = false;
-    // if (first[2]==curr_cpu_blocking_register){
-    //     is_first = true;
-    //     blocking_row = row;
-    //     blocking_column = stoi(first[1])%1024;
-    // }
-    int off = 1;
-    for(int i = 1;i<Dram_queue.size();i++){
-        vector<string> curr = Dram_queue[i];
-        int r = (stoi(curr[1])+CORE_MEMORY*stoi(curr[6]))/1024;
-        if(r==row){
-            Dram_queue.erase(Dram_queue.begin()+i);
-            Dram_queue.insert(off+Dram_queue.begin(),curr);
-            off++;
-        }
-        // if (curr[2]==curr_cpu_blocking_register){
-        //     blocking_row = r;
-        //     blocking_column = stoi(curr[1])%1024;
-        // }       
-    }
-    // if(blocking_row!= -1 && !is_first){
-    //     if(blocking_row == row){
-    //         int off2 = 1;
-    //         for(int i = 1;i<Dram_queue.size();i++){
-    //             vector<string> curr = Dram_queue[i];
-    //             int r = stoi(curr[1])/1024;
-    //             int c = stoi(curr[1])%1024;
-    //             if(c == blocking_column&&r == blocking_row){
-    //                 Dram_queue.erase(Dram_queue.begin()+i);
-    //                 Dram_queue.insert(off2+Dram_queue.begin(),curr);
-    //                 off2++;
-    //             }
-    //         }
-    //     }else{
-    //      for(int i = 1;i<Dram_queue.size();i++){
-    //             vector<string> curr = Dram_queue[i];
-    //             int r = stoi(curr[1])/1024;   
-    //             if(r==blocking_row){
-    //                 Dram_queue.erase(Dram_queue.begin()+i);
-    //                 Dram_queue.insert(off+Dram_queue.begin(),curr);
-    //                 off++;
-    //                 break;
-    //             }                
-    //         }
-    //     }
-    // }
-}    
-
 bool checker(int i,int file_number){
     if(all_pc[file_number]>=num_array[file_number]){return false;}
+    string ins = all_params[file_number][i][0];
+    if(((ins =="lw"||ins =="sw") && request_issued) || Dram_queue[file_number].size()>individual_queue_size){
+        return false;
+    }
     for (auto j :all_params[file_number][i]){
         if (all_blocking_registers[file_number][j]>0){
             return false;
@@ -376,7 +331,9 @@ int main(int argc, char *argv[]) {
     all_regs.resize(N);
     all_blocking_registers.resize(N);
     num_array.resize(N);
-    int rows_for_a_core = (1024/N);
+    Dram_queue.resize(N);
+    individual_queue_size = TOTAL_QUEUE_SIZE/N;
+    rows_for_a_core = (1024/N);
     CORE_MEMORY = 1024*rows_for_a_core;
     vector<string> input_files;
     fstream files[N];
@@ -483,111 +440,31 @@ int main(int argc, char *argv[]) {
         //if (pc<num && label.find(params[pc][0])!=label.end()){pc++;continue;}
         clock_cycle++;
         out<<"\n"<<"Clock Cycle "<<clock_cycle<<" -> "<<endl;
-        //out<<" Current CPU Blocking "<<curr_cpu_blocking_register<<endl;
-        // for (auto const & string_vec : Dram_queue) {
-        //     out<<"(";
-        //     for (auto const & str : string_vec) {
-        //         out << str<<", ";
-        //     }
-        //     out<<")";
-        // }
-        // out<<endl;
-        // out<<endl;
-        // for(auto x:Blocking_registers){
-        //     out<<"( ";
-        //     out <<x.first<<","<<x.second;
-        //     out<<" ) ";
-        // }
-        
-        bool flag = false;
-        pair<int,string> removed_register;
-        //DRAM 
-        if (!Dram_queue.empty()){
-            vector<string> Curr_executed = Dram_queue[0];
-            int address = stoi(Curr_executed[1]);
-            int row = (address+CORE_MEMORY*stoi(Curr_executed[6]))/1024;
-            int  col = (address+CORE_MEMORY*stoi(Curr_executed[6]))%1024;
-            if(address >= CORE_MEMORY|| col%4 !=0){
-                cout<<"Program tried to access invalid memory address "<<address<<endl;
-                exit(-1);
-            }
-            // for(auto k: Dram_queue[0]){
-            //      out<<k<<" ";
-            // }
-            // out<<endl;
-            while(true){
-                //out<<Dram_queue[0][3]<<endl;0
-                if (Dram_queue[0][3]=="0"){
-                    out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : Started "<<Dram_queue[0][0]<<" "<<Dram_queue[0][1]<<" "<<Dram_queue[0][2]<<" on Line "<<Dram_queue[0][5]<<endl;
-                    int curr = 0;
-                    if(Dram_queue[0][0]=="sw") row_buffer_updates++;
-                    if(last_buffer_row!=row){
-                        curr += ROW_ACCESS_DELAY;
-                        row_buffer_updates++;
-                        if (last_buffer_row!=-1){
-                            if(dirty_bit){
-                            curr += ROW_ACCESS_DELAY;
-                                out<<"Row "<<last_buffer_row<<" will be copied back to DRAM and ";
-                            }else{
-                                out<<"Row "<<last_buffer_row<<" need NOT be copied back to DRAM and "; 
-                            }
-                        }
-                        out<<"Row "<<row<<" will be activated\n";
-                        dirty_bit = 0;
-                    }
-                    curr += COL_ACCESS_DELAY;
-                    Dram_queue[0][4] = to_string(curr);
-                    Dram_queue[0][3] = "1";
-                    out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : Completed "<<"1"<<"/"<<Dram_queue[0][4]<<endl;
-                    break;
-                }
-                out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : Completed "<<to_string(stoi(Dram_queue[0][3])+1)<<"/"<<Dram_queue[0][4]<<endl;
-                if (stoi(Dram_queue[0][3])==ROW_ACCESS_DELAY-1){
-                    if(Dram_queue[0][4]==to_string(2*ROW_ACCESS_DELAY+COL_ACCESS_DELAY)){
-                        for (int j = 0;j<256;j++){
-                            if (Dram_Memory[last_buffer_row][j] != Row_buffer[j]){
-                                out<<"Memory at "<<last_buffer_row*1024+j*4<<" = "<<to_string(Row_buffer[j])<<endl;
-                            }
-                        }
-                        Dram_Memory[last_buffer_row] = Row_buffer;
-                    }
-                    if(stoi(Dram_queue[0][4])==ROW_ACCESS_DELAY+COL_ACCESS_DELAY){
-                        Row_buffer = Dram_Memory[row];
-                        last_buffer_row = row;
-                    }
-                    Dram_queue[0][3] = to_string(stoi(Curr_executed[3])+1);
-                    break;
-                }
-                if (stoi(Dram_queue[0][3])==2*ROW_ACCESS_DELAY-1 && Dram_queue[0][4]==to_string(2*ROW_ACCESS_DELAY+COL_ACCESS_DELAY)){
-                    Row_buffer = Dram_Memory[row];
-                    last_buffer_row = row;
-                    Dram_queue[0][3] = to_string(stoi(Curr_executed[3])+1);
-                    break;
-                }
 
-                if(stoi(Dram_queue[0][3])==stoi(Dram_queue[0][4])-1){
-                    total_int_executed++;
-                    if(Dram_queue[0][0]=="lw"){
-                        all_regs[stoi(Dram_queue[0][6])][Dram_queue[0][2]] = Row_buffer[col/4];
-                        flag  = true;
-                        removed_register = {stoi(Dram_queue[0][6]),Dram_queue[0][2]};
-                        out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : "<<removed_register.second<<" = "<<Row_buffer[col/4]<<endl;
-                        //Blocking_registers[Dram_queue[0][2]] -=1;
+        // MRM
+        if(request_issued){
+            if (pushed_from_core == executing_core){
+                while(true){
+                    if (Dram_queue[pushed_from_core].back()[7] == "0"){
+                        int x = Dram_queue[pushed_from_core].size();
+                        int firstrow = stoi(Dram_queue[pushed_from_core][0][1])/1024;
+                        int thisrow = stoi(Dram_queue[pushed_from_core].back()[1])/1024;
+                        if (firstrow != thisrow){request_issued = false;break;}
+                        Dram_queue[pushed_from_core].back()[7] == to_string(x-1);
+                        break;
+                    }else{
+                        int pos = stoi(Dram_queue[pushed_from_core].back()[7]);
+                        int prev_row = stoi(Dram_queue[pushed_from_core][pos-1][1])/1024;
+                        int curr_row = stoi(Dram_queue[pushed_from_core][pos][1])/1024;
+                        if(curr_row==prev_row){request_issued = false;break;}
+
                     }
-                    if(Dram_queue[0][0] =="sw"){
-                        Row_buffer[col/4] = stoi(Dram_queue[0][2]);
-                        dirty_bit = 1;
-                    }
-                    out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : Finished Instruction "<<Dram_queue[0][0]<<" "<<Dram_queue[0][1]<<" "<<Dram_queue[0][2]<<" on Line "<<Dram_queue[0][5]<<endl;
-                    Dram_queue.erase(Dram_queue.begin());
-                    sort_queue();
-                    //Dram_queue[0][3] = to_string(stoi(Dram_queue[0][3])+1);
-                    break;
                 }
-                Dram_queue[0][3] = to_string(stoi(Dram_queue[0][3])+1);
-                break;
             }
         }
+        //DRAM
+        
+ 
 
         //Normal Commands 
         for(int i=0;i<N;i++){
@@ -606,9 +483,10 @@ int main(int argc, char *argv[]) {
                     string relevant_registor = all_params[i][all_pc[i]][1];
                     int offset = stoi(all_params[i][all_pc[i]][2]);
                     int address = offset+all_regs[i][all_params[i][all_pc[i]][3]];
-                    Dram_queue.push_back({"lw",to_string(address),relevant_registor,"0","0",to_string(all_lines_numbers[i][all_pc[i]]),to_string(i)}); 
+                    Dram_queue[i].push_back({"lw",to_string(address),relevant_registor,"0","0",to_string(all_lines_numbers[i][all_pc[i]]),to_string(i),"0","0"}); 
+                    request_issued = true;
+                    pushed_from_core = i;
                     // ins,address,register/value,counter,total time,line number, file number
-                    sort_queue();  
                     all_blocking_registers[i][relevant_registor] ++;
                     out<<"File Number "<<i+1<< " : DRAM Request(Read) Issued for "<<"lw "<<address<<" "<<relevant_registor<<" on Line "<<all_lines_numbers[i][all_pc[i]]<<endl;        
                     all_pc[i]++;
@@ -618,8 +496,9 @@ int main(int argc, char *argv[]) {
                     string relevant_registor = all_params[i][all_pc[i]][1];
                     int offset = stoi(all_params[i][all_pc[i]][2]);
                     int address = offset+all_regs[i][all_params[i][all_pc[i]][3]];
-                    Dram_queue.push_back({"sw",to_string(address),to_string(all_regs[i][relevant_registor]),"0","0",to_string(all_lines_numbers[i][all_pc[i]]),to_string(i)});  
-                    sort_queue();
+                    Dram_queue[i].push_back({"sw",to_string(address),to_string(all_regs[i][relevant_registor]),"0","0",to_string(all_lines_numbers[i][all_pc[i]]),to_string(i)});
+                    request_issued = true;
+                    pushed_from_core = i;  
                     out<<"File Number "<<i+1<< " : DRAM Request(Write) Issued for "<<"sw "<<address<<" "<<all_regs[i][relevant_registor]<<" on Line "<<all_lines_numbers[i][all_pc[i]]<<endl;         
                     all_pc[i]++;
                     continue;
---
+++ A5/dump.cpp
@@ -0,0 +1,89 @@
+       bool flag = false;
+        pair<int,string> removed_register;
+        //DRAM 
+        if (!Dram_queue.empty()){
+            vector<string> Curr_executed = Dram_queue[0];
+            int address = stoi(Curr_executed[1]);
+            int row = (address+CORE_MEMORY*stoi(Curr_executed[6]))/1024;
+            int  col = (address+CORE_MEMORY*stoi(Curr_executed[6]))%1024;
+            if(address >= CORE_MEMORY|| col%4 !=0){
+                cout<<"Program tried to access invalid memory address "<<address<<endl;
+                exit(-1);
+            }
+            // for(auto k: Dram_queue[0]){
+            //      out<<k<<" ";
+            // }
+            // out<<endl;
+            while(true){
+                //out<<Dram_queue[0][3]<<endl;0
+                if (Dram_queue[0][3]=="0"){
+                    out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : Started "<<Dram_queue[0][0]<<" "<<Dram_queue[0][1]<<" "<<Dram_queue[0][2]<<" on Line "<<Dram_queue[0][5]<<endl;
+                    int curr = 0;
+                    if(Dram_queue[0][0]=="sw") row_buffer_updates++;
+                    if(last_buffer_row!=row){
+                        curr += ROW_ACCESS_DELAY;
+                        row_buffer_updates++;
+                        if (last_buffer_row!=-1){
+                            if(dirty_bit){
+                            curr += ROW_ACCESS_DELAY;
+                                out<<"Row "<<last_buffer_row<<" will be copied back to DRAM and ";
+                            }else{
+                                out<<"Row "<<last_buffer_row<<" need NOT be copied back to DRAM and "; 
+                            }
+                        }
+                        out<<"Row "<<row<<" will be activated\n";
+                        dirty_bit = 0;
+                    }
+                    curr += COL_ACCESS_DELAY;
+                    Dram_queue[0][4] = to_string(curr);
+                    Dram_queue[0][3] = "1";
+                    out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : Completed "<<"1"<<"/"<<Dram_queue[0][4]<<endl;
+                    break;
+                }
+                out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : Completed "<<to_string(stoi(Dram_queue[0][3])+1)<<"/"<<Dram_queue[0][4]<<endl;
+                if (stoi(Dram_queue[0][3])==ROW_ACCESS_DELAY-1){
+                    if(Dram_queue[0][4]==to_string(2*ROW_ACCESS_DELAY+COL_ACCESS_DELAY)){
+                        for (int j = 0;j<256;j++){
+                            if (Dram_Memory[last_buffer_row][j] != Row_buffer[j]){
+                                out<<"Memory at "<<last_buffer_row*1024+j*4<<" = "<<to_string(Row_buffer[j])<<endl;
+                            }
+                        }
+                        Dram_Memory[last_buffer_row] = Row_buffer;
+                    }
+                    if(stoi(Dram_queue[0][4])==ROW_ACCESS_DELAY+COL_ACCESS_DELAY){
+                        Row_buffer = Dram_Memory[row];
+                        last_buffer_row = row;
+                    }
+                    Dram_queue[0][3] = to_string(stoi(Curr_executed[3])+1);
+                    break;
+                }
+                if (stoi(Dram_queue[0][3])==2*ROW_ACCESS_DELAY-1 && Dram_queue[0][4]==to_string(2*ROW_ACCESS_DELAY+COL_ACCESS_DELAY)){
+                    Row_buffer = Dram_Memory[row];
+                    last_buffer_row = row;
+                    Dram_queue[0][3] = to_string(stoi(Curr_executed[3])+1);
+                    break;
+                }
+
+                if(stoi(Dram_queue[0][3])==stoi(Dram_queue[0][4])-1){
+                    total_int_executed++;
+                    if(Dram_queue[0][0]=="lw"){
+                        all_regs[stoi(Dram_queue[0][6])][Dram_queue[0][2]] = Row_buffer[col/4];
+                        flag  = true;
+                        removed_register = {stoi(Dram_queue[0][6]),Dram_queue[0][2]};
+                        out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : "<<removed_register.second<<" = "<<Row_buffer[col/4]<<endl;
+                        //Blocking_registers[Dram_queue[0][2]] -=1;
+                    }
+                    if(Dram_queue[0][0] =="sw"){
+                        Row_buffer[col/4] = stoi(Dram_queue[0][2]);
+                        dirty_bit = 1;
+                    }
+                    out<<"File Number "<<stoi(Dram_queue[0][6])+1<<" : Finished Instruction "<<Dram_queue[0][0]<<" "<<Dram_queue[0][1]<<" "<<Dram_queue[0][2]<<" on Line "<<Dram_queue[0][5]<<endl;
+                    Dram_queue.erase(Dram_queue.begin());
+                    sort_queue();
+                    //Dram_queue[0][3] = to_string(stoi(Dram_queue[0][3])+1);
+                    break;
+                }
+                Dram_queue[0][3] = to_string(stoi(Dram_queue[0][3])+1);
+                break;
+            }
+        }
